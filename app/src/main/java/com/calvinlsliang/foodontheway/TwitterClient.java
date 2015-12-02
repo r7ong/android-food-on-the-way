4abf2a39a871d1cc1555244097ffaa0f6fe29083
Calvin Liang <cliang@groupon.com>
2015-12-02 00:05:43 -0800

    Add support for Twitter login

---
+++ app/src/main/java/com/calvinlsliang/foodontheway/TwitterClient.java
@@ -0,0 +1,34 @@
+package com.calvinlsliang.foodontheway;
+
+import android.content.Context;
+
+import com.codepath.oauth.OAuthBaseClient;
+
+import org.scribe.builder.api.Api;
+import org.scribe.builder.api.TwitterApi;
+
+/*
+ * 
+ * This is the object responsible for communicating with a REST API. 
+ * Specify the constants below to change the API being communicated with.
+ * See a full list of supported API classes: 
+ *   https://github.com/fernandezpablo85/scribe-java/tree/master/src/main/java/org/scribe/builder/api
+ * Key and Secret are provided by the developer site for the given API i.e dev.twitter.com
+ * Add methods for each relevant endpoint in the API.
+ * 
+ * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
+ * 
+ */
+public class TwitterClient extends OAuthBaseClient {
+    public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class;
+    public static final String REST_URL = "https://api.twitter.com/1.1";
+    public static final String REST_CALLBACK_URL = "oauth://foodonthewayclient";
+
+    public static final String REST_CONSUMER_KEY = "REPLACE";       // Change this
+    public static final String REST_CONSUMER_SECRET = "REPLACE"; // Change this
+
+    public TwitterClient(Context context) {
+        super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
+    }
+
+}
